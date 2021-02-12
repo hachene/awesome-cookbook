6a09012ed37b2fc914d6a867f28d5b587554dfe6
hachene <jmp.juan@gmail.com>
2021-02-12 23:00:47 +0100

    :sparkles: Add link from recipe to home page

--- pages/recipes/recipe.tsx
+++ pages/recipes/recipe.tsx
@@ -1,3 +1,12 @@
+import Link from "next/link"
+
 export default function Recipe() {
-  return <h1>This is an awesome recipe</h1>
+  return (
+    <>
+      <h1>This is an awesome recipe</h1>
+      <Link href='/'>
+        <a>Back to home</a>
+      </Link>
+    </>
+  )
 }
