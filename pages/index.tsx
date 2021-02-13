c876529337c9b540501dff20c1685620843a958d
hachene <jmp.juan@gmail.com>
2021-02-13 14:44:45 +0100

    :sparkles: Adapt Index component to the new use of Layout

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,28 +1,16 @@
+import { Layout, siteTitle } from "../components/layout"
+import utilStyles from "../styles/utils.module.css"
 import Head from "next/head"
-import styles from "../styles/Home.module.css"
-import Link from "next/link"
-import Image from "next/image"
 
 export default function Home() {
   return (
-    <div className={styles.container}>
+    <Layout home>
       <Head>
-        <title>Awesome Cookbook</title>
-        <link rel='icon' href='/favicon.ico' />
+        <title>{siteTitle}</title>
       </Head>
-
-      <main className={styles.main}>
-        <h1 className={styles.title}>Hello production!</h1>
-        <Image
-          src='/images/profile.jpg'
-          height={144}
-          width={144}
-          alt='A dummy image showing a white abstract pattern'
-        />
-        <Link href='/recipes/recipe'>
-          <a>Go to the recipe</a>
-        </Link>
-      </main>
-    </div>
+      <section className={utilStyles.headingMd}>
+        <p>Your best recipes, in a single place!</p>
+      </section>
+    </Layout>
   )
 }
