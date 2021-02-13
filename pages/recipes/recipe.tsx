41b9fb39afd851f3b565a495a023793097e4e315
hachene <jmp.juan@gmail.com>
2021-02-13 11:16:37 +0100

    :sparkles: Add header to recipe page

--- pages/recipes/recipe.tsx
+++ pages/recipes/recipe.tsx
@@ -1,8 +1,13 @@
+import Head from "next/head"
 import Link from "next/link"
 
 export default function Recipe() {
   return (
     <>
+      <Head>
+        <title>Recipe</title>
+        <link rel='icon' href='/favicon.ico' />
+      </Head>
       <h1>This is an awesome recipe</h1>
       <Link href='/'>
         <a>Back to home</a>
