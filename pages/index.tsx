46470ada2e43f83b15b0ff1d6d071ad127bfc77c
hachene <jmp.juan@gmail.com>
2021-02-13 11:03:32 +0100

    :sparkles: Add a dummy image component to the home

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,6 +1,7 @@
 import Head from "next/head"
 import styles from "../styles/Home.module.css"
 import Link from "next/link"
+import Image from "next/image"
 
 export default function Home() {
   return (
@@ -12,6 +13,12 @@ export default function Home() {
 
       <main className={styles.main}>
         <h1 className={styles.title}>Hello production!</h1>
+        <Image
+          src='/images/profile.jpg'
+          height={144}
+          width={144}
+          alt='A dummy image showing a white abstract pattern'
+        />
         <Link href='/recipes/recipe'>
           <a>Go to the recipe</a>
         </Link>
