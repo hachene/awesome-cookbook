85fcbc081713b3b6693bba35548fb6ad2bccc671
hachene <jmp.juan@gmail.com>
2021-02-27 19:59:12 +0100

    :fire: Remove unused api directory

--- pages/api/hello.ts
+++
@@ -1,9 +0,0 @@
-// Next.js API route support: https://nextjs.org/docs/api-routes/introduction
-
-import { NextApiRequest, NextApiResponse } from 'next'
-
-const getName = (_: NextApiRequest, res: NextApiResponse) => {
-  res.status(200).json({ name: 'John Doe' })
-}
-
-export default getName
