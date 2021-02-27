f242bf86a2c1dc1e1857e152c9876557561b40e3
hachene <jmp.juan@gmail.com>
2021-02-27 20:23:24 +0100

    :pencil2: Fix typo in test name

--- test/pages/index.test.tsx
+++ test/pages/index.test.tsx
@@ -2,7 +2,7 @@ import { render, screen } from '@testing-library/react'
 import Home from '../../pages'
 
 describe('index', () => {
-  it('index renders the welcome message', () => {
+  it('renders the welcome message', () => {
     render(<Home allRecipesData={[]} />)
     const linkElement = screen.queryByText(/Your best recipes, in a single place!/)
     expect(linkElement).toBeVisible()
