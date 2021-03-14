57c08643300a6caa2f6e1e5415ccc18e537fb1c1
hachene <jmp.juan@gmail.com>
2021-03-14 21:36:45 +0100

    :fire: Remove unused test directory

--- test/pages/index.test.tsx
+++
@@ -1,9 +0,0 @@
-import { render } from "@testing-library/react"
-import Home from "../../pages"
-
-describe("Index", () => {
-  it("Index renders the welcome message", () => {
-    const app = render(<Home allRecipesData={[]} />)
-    expect(app.queryByText(/Your best recipes, in a single place!/)).toBeVisible()
-  })
-})
