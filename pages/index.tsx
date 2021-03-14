49bd059ccef693ccf5096ef2b95f801e0e05062d
hachene <jmp.juan@gmail.com>
2021-03-14 11:28:58 +0100

    :construction: Small change to trigger actions

--- pages/index.tsx
+++ pages/index.tsx
@@ -17,7 +17,7 @@ export default function Home({ allRecipesData }: HomeProps) {
         <title>{siteTitle}</title>
       </Head>
       <section className={utilStyles.headingMd}>
-        <p>Your best recipes, in a single place!</p>
+        <p>Your best recipes, in a single place!!</p>
       </section>
       <section className={`${utilStyles.headingMd} ${utilStyles.padding1px}`}>
         <h2 className={utilStyles.headingLg}>Blog</h2>
